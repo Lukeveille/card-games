1f8375974bc87205136d33b2f51618b5d6ae8843
Lukeveille <luke@lukeleveille.com>
2020-03-13 09:52:19 -0400

    cleaning up card display

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,84 @@
 import React from 'react';
 import { useState } from 'react';
+import heart from '../svg/heart.svg'
+import diamond from '../svg/diamond.svg'
+import spade from '../svg/spade.svg'
+import club from '../svg/club.svg'
 
 export default props => {
   const [flipped, setFlipped] = useState(false),
-  cardScale = .7,
+  [glow, setGlow] = useState(false),
+  cardScale = props.scale,
   innerWidth = cardScale * 9,
   innerHeight = cardScale * 14,
   border = cardScale * .8,
+  number = props.value[0] === 1? 'A' : props.value[0] === 11? 'J' : props.value[0] === 12? 'Q' : props.value[0] === 13? 'K' : props.value[0],
+  suit = props.value[1] === 'H'? heart : props.value[1] === 'D'? diamond : props.value[1] === 'S'? spade : club,
+  suitStyle = {
+    fontSize: `${cardScale * 1.75}em`,
+    margin: props.value[0] === 10? `${cardScale * .14}rem ${cardScale * -.1}rem` : `${cardScale * .14}rem`,
+    color: props.value[1] === 'D' || props.value[1] === 'H'? '#c00' : '#000'
+  },
   cardWrapper = {
-    width: `${innerWidth + border * 2}em`,
-    height: `${innerHeight + border * 2}em`,
+    width: `${innerWidth + border * 2}rem`,
+    height: `${innerHeight + border * 2}rem`,
     borderRadius: `${cardScale * 10}px`,
     display: 'inline-block',
     position: 'relative',
     transition: 'transform .2s ease-in-out',
     transformStyle: 'preserve-3d',
-    transform: flipped? 'rotateY(180deg)' : 'none'
+    transform: flipped? 'rotateY(180deg)' : 'none',
+    boxShadow: glow? '0 0 1em #ff0' : '0 0 1em #000',
+    zIndex: glow? '10000' : 'auto'
   },
   cardFace = {
-    width: `${innerWidth}em`,
-    height: `${innerHeight}em`,
-    border: `${border}em solid #fff`,
+    width: `${innerWidth}rem`,
+    height: `${innerHeight}rem`,
+    border: `${border}rem solid #fff`,
     borderRadius: `${cardScale * 10}px`,
     margin: 'auto',
     background: '#a00',
-    cursor: 'pointer',
+    cursor: glow? 'pointer' : 'default',
     position: 'absolute',
     backfaceVisibility: 'hidden'
   },
-  cardBack = {...cardFace, background: '#fff', transform: 'rotateY(180deg)'};
+  cardBack = {...cardFace, background: '#fff', transform: 'rotateY(180deg)'},
+  suiteImage = size => (<img 
+    src={suit}
+    alt='suit'
+    style = {{
+      width: `${cardScale * size}em`
+    }}
+  />),
+  cardSuite = style => <div style={style}>
+    <p style={suitStyle}>{number}</p>
+    {suiteImage(1.5)}
+  </div>,
+  faceCard = 'FACE',
+  suiteFace = props.value[0] < 11? suiteImage(1) : faceCard;
 
   return (
     <div
       style={cardWrapper}
-      className='card'
-      onClick={() => setFlipped(true)}
+      onMouseOver={() => { if (!flipped && props.active) setGlow(true) }}
+      onMouseOut={() => { setGlow(false) }}
+      onClick={() => { setGlow(false); setFlipped(true) }}
     >
       <figure
         style={cardFace}
-      >
-        {52 - props.cardInDeck}
-      </figure>
+      ></figure>
       <figure
         style={cardBack}
       >
-        {props.value[0]}
-        <br />
-        {props.value[1]}
+        {cardSuite({textAlign: 'left'})}
+        {props.value[0] === 1? suiteImage(3) : suiteFace}
+        {cardSuite({
+          textAlign: 'right',
+          transform: 'rotateX(180deg)',
+          bottom: '1px',
+          right: '1px',
+          position: 'absolute'
+        })}
       </figure>
     </div>
   )
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Card from './Card.js';
 import { useState } from 'react';
+// import heartQueen from '../svg/queen_of_hearts.svg';
+// import diamondQueen from '../svg/queen_of_diamonds.svg';
+// import spadeQueen from '../svg/queen_of_spades.svg';
+// import clubQueen from '../svg/queen_of_clubs.svg';
+// import heartKing from '../svg/king_of_heart.svg';
+// import diamondKing from '../svg/king_of_diamond.svg';
+// import spadeKing from '../svg/king_of_spade.svg';
+// import clubKing from '../svg/king_of_club.svg';
+// import heartJack from '../svg/jack_of_heart.svg';
+// import diamondJack from '../svg/jack_of_diamond.svg';
+// import spadeJack from '../svg/jack_of_spade.svg';
+// import clubJack from '../svg/jack_of_club.svg';
 
 export default props => {
   const [deck] = useState(props.newDeck),
-  [cardInDeck, setCardInDeck] = useState(0),
   shuffle = a => {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -12,17 +23,14 @@ export default props => {
     }
     return a;
   };
-  shuffle(deck);
 
-  // useEffect(() => {
-  //   console.log(cardInDeck);
-  // }, [setCardInDeck])
+  shuffle(deck);
   
   return (
-    <Card
-      value={deck[cardInDeck]}
-      setCardInDeck={setCardInDeck}
-      cardInDeck={cardInDeck}
-    />
+    <div>
+      {deck.map((card, i) => {
+        return <Card key={`card${i}`} active={true} value={card} scale={.5}/>
+      })}
+    </div>
   )
 };
